36cc5635d340c60da29b88393dc9a5e26d4ad97a
Bernie Shishaye <bernie86@gmail.com>
2018-04-12 12:40:52 -0400

    first comm

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace netcoreapp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Credit}/{action=Index}/{id?}");
             });
         }
     }
