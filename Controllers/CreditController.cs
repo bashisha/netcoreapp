850be55fd178df07ef98071dd9326bb29c426f55
Bernie Shishaye <bernie86@gmail.com>
2018-04-12 12:20:50 -0400

    first

---
+++ Controllers/CreditController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcoreapp.Models;
+
+namespace netcoreapp.Controllers
+{
+    public class CreditController : Controller
+    {
+      
+        // GET: Credit
+        public IActionResult Index()
+        {
+            var model = new CreditCardViewModel
+            {
+                Name = "Test Credit Card",
+                Addresses = new string[] { "Address1", "Address2" }
+            };
+            return View(model);
+        }
+
+        public IActionResult BackToPrev()
+        {
+            return View();
+        }
+
+        public IActionResult Shipping()
+        {
+            var model = new CreditCardViewModel
+            {
+                Name = "Test Credit Card",
+                Addresses = new string[] { "Address1", "Address2" }
+            };
+            return View(model);
+        }
+
+        public IActionResult Confirmation()
+        {
+            var model = new CreditCardViewModel
+            {
+                Name = "Test Credit Card",
+                Addresses = new string[] { "Address1", "Address2" }
+            };
+            return View(model);
+        }
+
+        public void Finish()
+        {
+            var model = new CreditCardViewModel
+            {
+                Name = "Test Credit Card",
+                Addresses = new string[] { "Address1", "Address2" }
+            };
+            //return View(model);
+        }
+    }
+}
---
+++ Models/CreditCardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreapp.Models
+{
+    public class CreditCardViewModel
+    {
+        public CreditCardViewModel() { }
+        public string Name { get; set; }
+        public string[] Addresses { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string SelectedAddress { get; set; }
+        public bool IsDeliveryStandard { get; set; }
+
+    }
+}
